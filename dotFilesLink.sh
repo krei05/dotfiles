a97cbb7e58b769e40e72c5fc97a47933bff15540
okada-keiji <xenophy.keiji.okada@gmail.com>
2014-06-04 15:09:55 +0900

    jshintの設定ファイルを追加　シェルスクリプトも更新

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -2,4 +2,6 @@
 ln -s ~/gitrep/dotfiles/.vimrc ~/.vimrc
 ln -s ~/gitrep/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/gitrep/dotfiles/.vim ~/.vim
+ln -s ~/gitrep/dotfiles/.jshintrc ~/.jshintrc
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+
